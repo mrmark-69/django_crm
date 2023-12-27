c23b7f5f762e8819b8b4ec1b6f316bc15bbcd678
mrmark <mrmark@yandex.ru>
2023-12-28 00:38:29 +1000

    изменено settings

--- mycrm/mycrm/settings.py
+++ mycrm/mycrm/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv(
     "DJANGO_SECRET_KEY",
 )
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
