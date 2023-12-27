f4ade08392f4997a96002c5c9a861f6e3a1801f8
mrmark <mrmark@yandex.ru>
2023-12-28 00:30:25 +1000

    убрал секретный ключ джанго

--- mycrm/mycrm/settings.py
+++ mycrm/mycrm/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rq#4wj8%1##^rtm=wlpx6kz6^q^*p46(&ik-c(!9cd316sro5r'
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'mycrm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'crm_db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
