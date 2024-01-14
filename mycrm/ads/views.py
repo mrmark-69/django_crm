8e3dc693fc4387b730491bb2ee8839014fec3d60
mrmark <mrmark@yandex.ru>
2024-01-14 14:14:17 +1000

    ads/views chanched

--- mycrm/ads/views.py
+++ mycrm/ads/views.py
@@ -70,12 +70,12 @@ class StatisticListView(ListView):
 
     def get_queryset(self):
         queryset = Advertisement.objects.annotate(
-            leads_count=Count('lead__pk', distinct=True),  # Подсчитываем количество лидов
-            customers_count=Count('lead__customer', distinct=True),  # Подсчитываем количество активных клиентов
-            products_sum=Sum('product__price', distinct=True),  # Подсчитываем стоимость продукта
+            leads_count=Count('lead__pk', distinct=True),
+            customers_count=Count('lead__customer', distinct=True),
+            products_sum=Sum('product__price', distinct=True),
             profit=ExpressionWrapper(
                 F('products_sum') / F('advertisement_budget'),
-                output_field=DecimalField(),  # Считаем соотношение контрактов к затратам
+                output_field=DecimalField(),
             )
         ).order_by('campaign_name')
 
