7815ed348280d3561e1321334d6d56c280ad8f0e
mrmark <mrmark@yandex.ru>
2024-01-15 21:00:36 +1000

    сократил запросы к базе

--- mycrm/customers/models.py
+++ mycrm/customers/models.py
@@ -1,14 +1,21 @@
 from django.db import models
-from django.db.models import Manager
+from django.db.models import Manager, Q
 
 from contracts.models import Contract
 from leads.models import Lead
 
 
+def get_available_contracts():
+    used_contracts = Customer.objects.exclude(contract__isnull=True).values_list('contract', flat=True)
+    return Q(pk__in=Contract.objects.exclude(pk__in=used_contracts))
+
+
 class Customer(models.Model):
     objects = Manager()
-    lead = models.OneToOneField(Lead, on_delete=models.CASCADE, )
-    contract = models.OneToOneField(Contract, on_delete=models.CASCADE)
+    lead = models.OneToOneField(Lead, on_delete=models.CASCADE,
+                                limit_choices_to=Q(customer__isnull=True))
+    contract = models.OneToOneField(Contract, on_delete=models.CASCADE,
+                                    limit_choices_to=get_available_contracts)
 
     def __str__(self) -> str:
         # noinspection PyUnresolvedReferences
--- mycrm/customers/views.py
+++ mycrm/customers/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -12,14 +12,15 @@ class CustomersListView(ListView):
     context_object_name = 'customers'
 
 
-class CustomerDetailView(PermissionRequiredMixin, DetailView):
-    permission_required = 'customers.view_customer'
+class CustomerDetailView(DetailView):
     queryset = Customer.objects.select_related('lead', 'contract')
     template_name = 'customers/customers-detail.html'
 
 
-class CustomerCreateView(PermissionRequiredMixin, CreateView):
-    permission_required = 'customers.add_customer'
+class CustomerCreateView(UserPassesTestMixin, CreateView):
+    def test_func(self):
+        user = self.request.user
+        return user.is_superuser or user.has_perm('customers.add_customer')
 
     model = Customer
     fields = '__all__'
@@ -27,8 +28,10 @@ class CustomerCreateView(PermissionRequiredMixin, CreateView):
     success_url = reverse_lazy('customers:customers_list')
 
 
-class CustomerUpdateView(PermissionRequiredMixin, UpdateView):
-    permission_required = 'customers.change_customer'
+class CustomerUpdateView(UserPassesTestMixin, UpdateView):
+    def test_func(self):
+        user = self.request.user
+        return user.is_superuser or user.has_perm('customers.change_customer')
 
     model = Customer
     fields = '__all__'
@@ -41,10 +44,13 @@ class CustomerUpdateView(PermissionRequiredMixin, UpdateView):
         )
 
 
-class CustomerDeleteView(PermissionRequiredMixin, DeleteView):
-    permission_required = 'customers.delete_customer'
+class CustomerDeleteView(UserPassesTestMixin, DeleteView):
+    def test_func(self):
+        user = self.request.user
+        return user.is_superuser or user.has_perm('customers.delete_lead')
 
     model = Customer
     form_class = ConfirmForm
     template_name = 'customers/customers-delete.html'
     success_url = reverse_lazy('customers:customers_list')
+
